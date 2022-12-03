233128c2ff6b5fb03d21fafb0c2b1f1083c68ed7
MythosOne <107143884+MythosOne@users.noreply.github.com>
2022-12-03 20:04:45 +0200

    goit-js-hw-11-final

--- src/apiService.js
+++ src/apiService.js
@@ -1,30 +1,11 @@
-import Notiflix from 'notiflix';
-
-// const loadMoreBtn = document.querySelector('.load-more');
-// loadMoreBtn.addEventListener('click', onLoadMore);
-// loadMoreBtn.setAttribute('disabled', true);
-
-// let searchQuery = '';
-// let page = 1;
+import axios from 'axios';
 
 export default async function apiService(searchQuery, page) {
     const API_KEY = '31642520-d6a6357411a55db3459510987';
     const BASE_URL = 'https://pixabay.com/api/';
 
-    return await fetch(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&${page}`)
+    return await axios(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`)
         .then(resp => {
-            if (!resp.ok) {
-                throw new Error();
-            }
-            return resp.json();
+            return resp.data;
         });
 }
-
-// function onLoadMore() {
-//     page += 1;
-//     apiService(searchQuery, page).then(data => { createGallery.insertAdjacentHTML('beforeend', createMarkup(data.hits)); });
-// }
-
-// function resetPage() {
-//     page = 1;
-// }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import Notiflix from 'notiflix';
-import axios from 'axios';
 import './css/styles.css';
-
 import apiService from './apiService.js';
 
 //https://pixabay.com/api/docs/
@@ -11,57 +9,72 @@ import apiService from './apiService.js';
 const searchForm = document.querySelector('.search-form');
 const createGallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
-loadMoreBtn.addEventListener('click', onLoadMore);
 
 searchForm.addEventListener('submit', onSearch);
+searchForm.addEventListener('input', inputValue);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
 loadMoreBtn.style.display='none';
 
 let searchQuery = '';
 let page = 1;
 
-function onSearch(event) {
-    event.preventDefault();
-    const searchQuery = event.target.elements.searchQuery.value;
-
-    if (!searchQuery) {
+function inputValue(event) {
+    const searchValue = event.target.value;
+    if (!searchValue) {
         createGallery.innerHTML = '';
+        loadMoreBtn.style.display = 'none';
         return;
     }
-    
+}
+
+function onSearch(event) {
+    event.preventDefault();
+    searchQuery = event.target.searchQuery.value;
+
     apiService(searchQuery, page).then(data => console.log(data));
 
-    apiService(searchQuery, page).then(data => {
-        if (!data.hits.length) {
-            Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
-        } else {
-            loadMoreBtn.style.display='block';
-        }
-    });
-    
-    apiService(searchQuery, page).then(data => { createGallery.insertAdjacentHTML('beforeend', createMarkup(data.hits)); });
+    apiService(searchQuery, page)
+        .then(data => {
+            if (!data.hits.length) {
+                Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+            } else {
+                Notiflix.Notify.info(`Hooray! We found totalHits ${data.totalHits} images.`);
+                loadMoreBtn.style.display = 'block';
+            }
+            createGallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+        });
 }
 
 function onLoadMore() {
     page += 1;
-    apiService(searchQuery, page).then(data => { createGallery.insertAdjacentHTML('beforeend', createMarkup(data.hits)); });
+    apiService(searchQuery, page).then(data => {
+    console.log(data.hits.length*page);
+        createGallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+        if (data.hits.length*page > data.totalHits) {
+            Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`);
+            loadMoreBtn.style.display = 'none';
+        }
+    });
 }
 
 function createMarkup(arr) {
-    return arr.map(({webformatURL, tags, likes, views, comments, downloads}) => `<div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" width="240px" loading="lazy" />
+    return arr.map(({ webformatURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
+    <div class="thumb">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </div>
     <div class="info">
         <p class="info-item">
-            <b>Likes ${likes}</b>
+            <b>Likes: ${likes}</b>
         </p>
         <p class="info-item">
-            <b>Views ${views}</b>
+            <b>Views: ${views}</b>
         </p>
         <p class="info-item">
-            <b>Comments ${comments}</b>
+            <b>Comments: ${comments}</b>
         </p>
         <p class="info-item">
-            <b>Downloads ${downloads}</b>
+            <b>Downloads: ${downloads}</b>
         </p>
     </div>
 </div>`).join('');
